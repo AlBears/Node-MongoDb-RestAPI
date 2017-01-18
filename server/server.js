430bf132f09e5d06ac58842bac224f58ba080ca6
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-18 11:34:56 +0300

    private route with middleware

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
+var { authenticate } = require('./middleware/authenticate');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -110,6 +112,13 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+
+
+/* Private route with middleware*/
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 
